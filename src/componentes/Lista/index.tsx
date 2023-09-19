49e9e81b7c8f37a2e5b369c4483bb104548db2ed
Felipe Suriz <felipesuriz@hotmail.com>
2023-09-19 08:54:45 -0300

    Introduzindo estados

--- src/componentes/Lista/index.tsx
+++ src/componentes/Lista/index.tsx
@@ -1,25 +1,30 @@
+import { useState } from 'react';
 import Item from './Item';
 import styles from './Lista.module.scss';
 
 function Lista() {
-    const tarefas = [
-        {
-            tarefa: 'React',
-            tempo: '02:00:00'
-        },
-        {
-            tarefa: 'JavaScript',
-            tempo: '01:00:00'
-        },
-        {
-            tarefa: 'TypeScript',
-            tempo: '03:00:00'
-        }
-    ];
+    const [tarefas, setTarefas] = useState(
+        [
+            {
+                tarefa: 'React',
+                tempo: '02:00:00'
+            },
+            {
+                tarefa: 'JavaScript',
+                tempo: '01:00:00'
+            },
+            {
+                tarefa: 'TypeScript',
+                tempo: '03:00:00'
+            }
+        ]
+    );
 
     return (
         <aside className={styles.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTarefas([...tarefas, { tarefa: 'Estudar estado', tempo: '05:00:00' }]);
+            }}>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item
