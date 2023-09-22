136743356004781fff402a6f2f4c0b0101c58a87
Felipe Suriz <felipesuriz@hotmail.com>
2023-09-22 10:57:27 -0300

    Regressiva

--- src/componentes/Botao/index.tsx
+++ src/componentes/Botao/index.tsx
@@ -4,11 +4,12 @@ import styles from './Botao.module.scss';
 class Botao extends React.Component<{
     children: React.ReactNode,
     type?: "button" | "submit" | "reset" | undefined
+    onClick?: () => void
 }> {
     render() {
-        const { type = 'button' } = this.props;
+        const { type = 'button', onClick } = this.props;
         return (
-            <button type={type} className={styles.botao}>
+            <button onClick={onClick} type={type} className={styles.botao}>
                 {this.props.children}
             </button>
         )
--- src/componentes/Cronometro/index.tsx
+++ src/componentes/Cronometro/index.tsx
@@ -18,13 +18,22 @@ export default function Cronometro({ selecionado }: Props) {
         }
     }, [selecionado])
 
+    function regressiva(contador: number = 0) {
+        setTimeout(() => {
+            if (contador > 0) {
+                setTempo(contador - 1)
+                return regressiva(contador -1)
+            }
+        }, 1000)
+    }
+
     return (
         <div className={styles.cronometro}>
             <p className={styles.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={styles.relogioWrapper}>
-                <Relogio tempo={tempo}/>
+                <Relogio tempo={tempo} />
             </div>
-            <Botao>
+            <Botao onClick={() => regressiva(tempo)}>
                 Começar!
             </Botao>
         </div>
